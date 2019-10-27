4e123e257a3f9c2332f0c8223b2f299cddf62be7
ɐ ʞ s ɥ ɐ ʎ <ijoshi90@users.noreply.github.com>
2019-10-27 20:40:41 +0530

    commit

---
+++ Python/area_of_circle.py
@@ -0,0 +1,10 @@
+"""
+Author : Akshay Joshi
+GitHub : https://github.com/ijoshi90
+Created on 27-Oct-19 at 20:35
+"""
+
+from math import *
+
+radius = float(input("Enter Radius of Circle : "))
+print ("Area of the Circle with Radius {} is {}".format(radius,pi*radius*radius))
