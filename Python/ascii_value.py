72efa263fbbe86d37f10fdb155c91227da6dfb18
ɐ ʞ s ɥ ɐ ʎ <ijoshi90@users.noreply.github.com>
2019-10-27 20:45:24 +0530

    commit

---
+++ Python/ascii_value.py
@@ -0,0 +1,8 @@
+"""
+Author : Akshay Joshi
+GitHub : https://github.com/ijoshi90
+Created on 27-Oct-19 at 20:41
+"""
+
+character = input("Enter a key to find its ASCII value : ")
+print (ord(character))
