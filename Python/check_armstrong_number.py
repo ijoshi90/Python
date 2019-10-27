da8eeaa8f630adbae9a0b8f0cc2bb26b1613a7f8
ɐ ʞ s ɥ ɐ ʎ <ijoshi90@users.noreply.github.com>
2019-10-27 20:18:37 +0530

    commit

---
+++ Python/check_armstrong_number.py
@@ -0,0 +1,24 @@
+"""
+Author : Akshay Joshi
+GitHub : https://github.com/ijoshi90
+Created on 27-Oct-19 at 19:04
+"""
+
+number = int(input("Enter the number to check if its Armstrong Number or Not : "))
+number_list = [] # Empty list
+string_number = str(number)
+
+# Split the numbers in to list
+for i in range(len(string_number)):
+    number_list.append(string_number[i])
+
+# Calculate the sum of all the numbers
+total = 0
+for i in number_list:
+    num = int(i)
+    total = total + (num*num*num)
+
+if number == total:
+    print ("{} is an Armstrong Number".format(number))
+else:
+    print ("{} is not an Armstrong Number".format(number))
