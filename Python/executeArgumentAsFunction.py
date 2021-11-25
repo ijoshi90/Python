02be201503e50c7d5b4cf17b346352be8f6206c5
Akshay Joshi <akshay.joshi.m@gmail.com>
2021-11-25 13:49:48 +0100

    Add files via upload

---
+++ Python/executeArgumentAsFunction.py
@@ -0,0 +1,35 @@
+# Reads the argument passed and calls the respective function
+# Usage :
+# python executeWithArguments.py functionsAB
+# ---- or ----
+# python executeWithArguments.py functionsCD
+import sys
+
+functionToBeExecuted = sys.argv[1] + "()"
+
+def functionsAB():
+    def functionA():
+        print ("Function A is called")
+
+    def functionB():
+        print ("Function B is called")
+
+    functionA()
+    functionB()
+
+def functionsCD():
+    def functionC():
+        print("Function C is called")
+
+    def functionD():
+        print("Function D is called")
+
+    functionC()
+    functionD()
+
+# Takes the 1st argument provided executes it
+try:
+    exec (functionToBeExecuted)
+except:
+    print("Wrong argument provided while executing the function")
+    print("Supported arguments functionsAB or functionsCD")
